7be2526003db6a51e1ece4f503998609d93d6ecb
Matt Glazar <matt.glazar@gmail.com>
2011-09-10 22:13:04 -0700

    ApplicationDomainy thing

--- client/main.js
+++ client/main.js
@@ -7,6 +7,9 @@ function main(stage) {
         if (gameView && beat && Game) {
             sp.bridge.send('doneloading', null, null, null);
 
+            gameView.x = stage.stageWidth / 2;
+            gameView.y = stage.stageHeight / 2;
+
             stage.addChild(gameView);
             new Game(gameView, [ beat ]);
         }
@@ -28,7 +31,7 @@ function main(stage) {
     var loader = new sp.Loader();
     var cli = loader.contentLoaderInfo;
     cli.addEventListener(sp.Event.COMPLETE, function (event) {
-        gameView = cli.content;
+        gameView = new cli.applicationDomain.definitions.Game();
 
         checkDoneLoading();
     });
